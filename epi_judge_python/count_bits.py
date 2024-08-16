1fd678e6c233d6c226ea768a796b239d8156ffc5
bluechry <bluechry@gmail.com>
2024-08-16 19:19:19 +0900

    Pass count_bits.py

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -1,12 +1,41 @@
 from test_framework import generic_test
 
 
-def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+def count_bits_1(x: int) -> int:
+    count = 0
+    while x:
+        count += x & 1
+        x >>= 1
+    return count
+
+
+def count_bits_2(x: int) -> int:
+    count = 0
+    while x:
+        count += 1
+        x &= x - 1
+    return count
+
+
+def count_bits_3(x: int) -> int:
+    return bin(x).count('1')
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
-                                       count_bits))
+    generic_test.generic_test_main(
+        'count_bits.py',
+        'count_bits.tsv',
+        count_bits_1
+    )
+
+    generic_test.generic_test_main(
+        'count_bits.py',
+        'count_bits.tsv',
+        count_bits_2
+    )
+
+    generic_test.generic_test_main(
+        'count_bits.py',
+        'count_bits.tsv',
+        count_bits_3
+    )
