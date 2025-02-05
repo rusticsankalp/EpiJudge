f1d8c4beacc2389f5db9b0144e2dd9f0ee1caf68
Sankalp Gupta <rusticsankalp@gmail.com>
2025-02-04 20:24:23 -0800

    next pertumation

--- epi_judge_cpp/next_permutation.cc
+++ epi_judge_cpp/next_permutation.cc
@@ -1,10 +1,24 @@
 #include <vector>
+#include <algorithm>
+
+using namespace std;
 
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> NextPermutation(vector<int> perm) {
-  // TODO - you fill in here.
-  return {};
+	auto inversion_point = is_sorted_until(rbegin(perm), rend(perm));
+	if (inversion_point == rend(perm))
+	{
+		return {};
+	}
+
+	auto replacement_candidate = upper_bound(rbegin(perm), inversion_point, *inversion_point);
+
+	iter_swap(inversion_point, replacement_candidate);
+
+	reverse(rbegin(perm), inversion_point);
+
+	return perm;
 }
 
 int main(int argc, char* argv[]) {
